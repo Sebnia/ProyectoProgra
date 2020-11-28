24222f9f05dfa95d918c263eab9b40a485886e4d
Renzo Salinas <renzos.f15@gmail.com>
2020-11-28 01:43:33 -0500

    modificando observaciones de clase

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,29 +22,15 @@ namespace ProyectoProgra.Controllers
         {
             return View();
         }
+
+        public IActionResult contactenos()
+        {
+            return View();
+        }
  
         public IActionResult nosotros()
         {
             return View();
-        }
-
-        public IActionResult RegistrarUsu()
-        {
-            var model = new RegistrarUsu();
-            model.District = " ";
-            return View(model);            
-        }
-
-        [HttpPost]
-        public IActionResult RegistrarUsu(RegistrarUsu registrarUsu)
-        {
-            if (ModelState.IsValid) 
-            {
-                // TODO: Hacer algo con los parámetros del objecto contacto
-                return RedirectToAction("Index");
-            }
-            
-            return View(registrarUsu);
         }
 
 
